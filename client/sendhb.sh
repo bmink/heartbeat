5f3a4b8e708f5a132adfb6a31dd56d8bcceb701f
Barna Mink <barnamink@gmail.com>
2021-02-23 18:40:08 -0800

    .

---
+++ client/sendhb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+IDFILE=.idstr
+URL=http://192.168.1.128/cgi-bin/rpihbsrv.cgi
+
+if [ ! -f "$IDFILE" ]; then
+        echo "No file found at $IDFILE"
+        exit 1
+fi
+
+IDSTR=`cat $IDFILE`
+FREE_OUT=`free -h`
+UPTIME_OUT=`uptime`
+VMSTAT_OUT=`vmstat -S M`
+
+
+echo "$POSTDATA"
+
+echo -n "$POSTDATA" | curl -X POST -H "Content-Type: text/plain" --data-urlencode "idstr=$IDSTR" --data-urlencode "free_output=$FREE" --data-urlencode "uptime_output=$UPTIME_OUT" --data-urlencode "vmstat_output=$VMSTAT_OUT" "$URL"
