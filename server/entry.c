f6c5c60bcc92d031cdb0d64312da619ccdb7974b
Barna Mink <barnamink@gmail.com>
2021-02-26 14:28:18 -0800

    .

--- server/entry.c
+++ server/entry.c
@@ -9,15 +9,37 @@ static entry_t *_entry_init(void);
 entry_t *
 entry_init_frompostdata(const char *ipaddr, const char *postdata)
 {
-	entry_t	*entry;
-	int	err = 0;
+	entry_t		*entry;
+	int		err;
+	int		ret;
 
 	err = 0;
 	entry = NULL;
 
+	if(xstrempty(ipaddr) || xstrempty(postdata)) {
+		err = EINVAL;
+		goto end_label;
+	}
+
 	entry = _entry_init();
 	if(entry == NULL) {
 		err = ENOMEM;
+		goto end_label;
+	}
+
+	ret = getval("hostn", postdata, entry->en_name);
+	if(ret != 0) {
+		blogf("Could not get hostn");
+		err = ret;
+		goto end_label;
+	}
+
+	bstrcat(entry->en_ipaddr, ipaddr);
+
+	ret = getval("free_output", postdata, entry->en_free_outp);
+	if(ret != 0) {
+		blogf("Could not get free_output");
+		err = ret;
 		goto end_label;
 	}
 
@@ -131,7 +153,65 @@ entry_uninit(entry_t **entryp)
 int
 getval(const char *nam, const char *buf, bstr_t *dst)
 {
-	return 0;
+	const char	*cur;
+	bstr_t		*srchstr;
+	int		err;
+	int		pos;
+	bstr_t		*encoded;
+	int		ret;
+
+	err = 0;
+	srchstr = NULL;
+
+	if(xstrempty(nam) || xstrempty(buf) || dst == NULL)
+		return EINVAL;
+
+	srchstr = binit();
+	if(srchstr == NULL) {
+		blogf("Could not allocate srchstr");
+		err = ENOMEM;
+		goto end_label;
+	}
+
+	bprintf(srchstr, "%s=", nam);
+
+	pos = xstrstr(buf, bget(srchstr));
+	if(pos < 0) {
+		blogf("Field not found: %s", nam);
+		err = ENOMEM;
+		goto end_label;
+	}
+
+	cur = buf + pos + bstrlen(srchstr);
+
+	encoded = binit();
+	if(encoded == NULL) {
+		blogf("Could not allocate encoded");
+		err = ENOMEM;
+		goto end_label;
+	}
+
+	for(; *cur && *cur != '&'; ++cur) {
+		bputc(encoded, *cur);
+	}
+
+	printf("encoded: %s\n", bget(encoded));
+
+	ret = burldecode(bget(encoded), dst);
+	if(ret != 0) {
+		blogf("Could not urldecode value: %s", strerror(ret));
+		err = ret;
+		goto end_label;
+	}
+	
+	printf("decoded: %s\n", bget(dst));
+
+end_label:
+
+	buninit(&srchstr);
+	buninit(&encoded);
+
+	return err;
 }
 
 
